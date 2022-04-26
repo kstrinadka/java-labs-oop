9b4a6f1f8e9a846d9d4a67294445fba104dc48db
kstrinadka <k.strinadka@gmail.com>
2022-04-26 22:58:12 +0700

    added some interfaces

--- lab3/src/main/java/com/strinadkakirill/javalabs/lab3_minesweeper/lab3/Model/Cell/CellState.java
+++ lab3/src/main/java/com/strinadkakirill/javalabs/lab3_minesweeper/lab3/Model/Cell/CellState.java
@@ -4,5 +4,12 @@ package com.strinadkakirill.javalabs.lab3_minesweeper.lab3.Model.Cell;
 
 // здесь будет храниться состояние ячейки (открыта, закрыта, флажок)
 // и есть ли там мина или нет
-public class CellState {
+
+/**
+ * каждая клеточка имеет свое состояние
+ * @param condition - открыта, закрыта, флажок
+ * @param hasMine - есть ли мина
+ */
+public record CellState(CellConditions condition, boolean hasMine) {
+
 }
